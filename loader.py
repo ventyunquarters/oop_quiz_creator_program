470877dbd4a4f5888c6386c17922f818fde06931
unknown <airaevillarito@iskolarngbayan.pup.edu.ph>
2025-05-29 00:19:27 +0800

    Started with the loader of the quiz naming it as Quiz loader class

---
+++ loader.py
@@ -0,0 +1,16 @@
+from question import Question
+
+class QuizLoader:
+
+    def load_quiz(filename):
+        with open(filename, 'r') as file:
+            raw_entries = file.read().strip().split('\n\n')
+
+        quiz_data = []
+        for entry in raw_entries:
+            lines = entry.strip().split('\n')
+            question = lines[0][3:]
+            answer = lines[1][3:]
+            choices = lines[2][3:].split(';')
+            quiz_data.append(Question(question, answer, choices))
+        return quiz_data
